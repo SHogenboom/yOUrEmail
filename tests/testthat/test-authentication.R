8c937ad3ca1ecfc3a693990d9b0111406ac44f4e
Sally Hogenboom <sally.hogenboom@gmail.com>
2022-04-20 14:42:33 +0200

    TEST: Authentication

---
+++ tests/testthat/test-authentication.R
@@ -0,0 +1,9 @@
+#' Authentication
+
+testthat::test_that("Outlook: Authentication", {
+  # Authenticate & get Outlook Environment
+  outlook <- Microsoft365R::get_business_outlook()
+
+  # TO DO:
+  # Expect ms_outlook environment class
+})
