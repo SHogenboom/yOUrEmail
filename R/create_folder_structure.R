934050c9e702058340dfbea840e3e262d859d23c
Sally Hogenboom <sally.hogenboom@gmail.com>
2022-07-11 11:47:15 +0200

    Deal with non-existing course_runs

--- R/create_folder_structure.R
+++ R/create_folder_structure.R
@@ -27,21 +27,29 @@ create_folder_structure <- function(submission_info,
   }
 
   #### COURSE RUN ####
-  course_run_folder <- here::here(
-    course_folder,
-    submission_info$course_run
-  )
+  # Check if a course run is available, for some this is not the case then no
+  # ... course run folder should be created.
+  if (!is.na(submission_info$course_run)) {
+    course_run_folder <- here::here(
+      course_folder,
+      submission_info$course_run
+    )
 
-  # Some courses do not have different course runs,
-  # if the course_id is the same as the course_run we do not need the extra
-  # ... nested folder structure
-  if (submission_info$course_id != submission_info$course_run) {
-    # Check existence of the course folder,
-    # ... create it if it does not exist
-    if (!dir.exists(course_run_folder)) {
-      dir.create(course_run_folder)
-    }
-  }
+    # Some courses do not have different course runs,
+    # if the course_id is the same as the course_run we do not need the extra
+    # ... nested folder structure
+
+    if (submission_info$course_id != submission_info$course_run) {
+      # Check existence of the course folder,
+      # ... create it if it does not exist
+      if (!dir.exists(course_run_folder)) {
+        dir.create(course_run_folder)
+      } # END if dir.exists
+    } # END if course_id/course_run
+  } else {
+    course_run_folder <- course_folder
+  } # END if course_run
+
 
   #### STUDENT ####
   student_folder <- here::here(
