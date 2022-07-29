32bfabe976a82cab6ea03c91903479851b2b122d
Sally Hogenboom <sally.hogenboom@gmail.com>
2022-07-29 13:23:34 +0200

    replace graded by with empty date

--- R/get_submission_info.R
+++ R/get_submission_info.R
@@ -172,7 +172,8 @@ get_submission_info <- function(email,
   # Initialize for manual completion
   dat %<>%
     tibble::add_column("assignment" = "#") %>%
-    tibble::add_column("graded_on" = .$submission_date) %>%
+    tibble::add_column("graded_on" = lubridate::as_date(submission_date,
+                                                        format = "%d/%m/%Y")) %>%
     tibble::add_column("grade" = numeric(1)) %>%
     tibble::add_column("grading_notes" = character(1))
 
